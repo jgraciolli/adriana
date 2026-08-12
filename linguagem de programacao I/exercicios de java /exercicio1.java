8ba84ef722eba89c54e0b2b5a8c517f38f2681be
João Graciolli <148507173+jgraciolli@users.noreply.github.com>
2026-08-12 20:12:19 -0300

    feat: exercicio1

---
+++ linguagem de programacao I/exercicios de java /exercicio1.java
@@ -0,0 +1,30 @@
+// EXERCÍCIO 1
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        int n1 = 0;
+        int n2 = 0;
+        boolean multiplo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe dois números inteiros: ");
+        System.out.print("Número 1: ");
+        n1 = scanner.nextInt();
+
+        System.out.print("Número 2: ");
+        n2 = scanner.nextInt();
+
+        if (n2 == 0)
+            multiplo = false;
+        else
+            multiplo = (n1 % n2 == 0);
+
+        if (multiplo)
+            System.out.println("O primeiro número é múltiplo do segundo.");
+        else
+            System.out.println("O primeiro número não é múltiplo do segundo.");
+
+    }
+}
