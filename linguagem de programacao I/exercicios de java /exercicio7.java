d58c88fa07d7730884122446a5e52ed3d9d02781
João Graciolli <148507173+jgraciolli@users.noreply.github.com>
2026-08-12 21:09:31 -0300

    feat: exercicio7

---
+++ linguagem de programacao I/exercicios de java /exercicio7.java
@@ -0,0 +1,33 @@
+// EXERCÍCIO 7
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class exercicio7 {
+    public static void main(String[] args) {
+        int quantidadeDias, kmAdicional;
+        double kmPercorridos, limiteKm, precoAluguel;
+        double precoDiaria = 120;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Para calcular o custo total do aluguel de seu carro, insira os dados abaixo.");
+        System.out.print("Quantiade de dias alugados: ");
+        quantidadeDias = scanner.nextInt();
+
+        //limite definido seguindo um pensamento de 100km por dia mais uma folga (multiplicação por 1.25)
+        limiteKm = (quantidadeDias * 100) * 1.25;
+
+        System.out.print("Quantidade de quilômetros percorridos: ");
+        kmPercorridos = scanner.nextDouble();
+
+        precoAluguel = quantidadeDias * precoDiaria;
+        kmAdicional = (int) (kmPercorridos - limiteKm);
+
+        if (kmAdicional > 0)
+            precoAluguel += kmAdicional * 0.80;
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        System.out.print("O preço final de aluguel do carro resulta em: R$" + df.format(precoAluguel));
+
+    }
+}
